2ef9b25e4b22df62f0bd4c462c87e5469895deae
Danijel Bjegovic <danijel.bjegovic@performance-technologies.de>
2019-08-09 16:10:39 +0200

    expecting exception in tests

--- src/Queue.php
+++ src/Queue.php
@@ -1,10 +1,15 @@
 <?php
 class Queue
 {
+    public const MAX_ITEMS = 5;
     protected $items = [];
 
     public function push($item)
     {
+        if($this->getCount() == static::MAX_ITEMS)
+        {
+            throw new QueueException("Queue is full");
+        }
         $this->items[] = $item;
     }
     public function pop()
