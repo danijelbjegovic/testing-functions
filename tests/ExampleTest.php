accc6a8d1d8466e1c4ce3e953594a5b4c6dfce40
Danijel Bjegovic <danijel.bjegovic@performance-technologies.de>
2019-08-21 10:17:42 +0200

    implement mockeryTestCase

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,10 +1,12 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends MockeryTestCase
 {
     public function testAddingTwoPusTwoResultsInFour()
     {
         $this->assertEquals(4, 2 + 2);
     }
+
 }
